e539ee01eb92565f4c14fb8111df350b387b7e18
Mason Short <ironicallyshort737@hotmail.com>
2019-08-15 14:45:44 -0600

    minimax almost complete

--- sources/script/script.js
+++ sources/script/script.js
@@ -7,7 +7,7 @@ function gameState(board) {
     if (!isMovesLeft(board)) return false;
 
     // checks each row
-    board.foreach(element => {
+    board.forEach(element => {
         if(element.every(val => val === element[0])){
             if (element[0] === player) {
                 return -10;
@@ -41,34 +41,75 @@ function gameState(board) {
 
 function isMovesLeft(board){
     // checks for any available moves
-    board.foreach(element => {
+    board.forEach(element => {
        if(element.includes(0)) return true;
     });
     return false;
 }
 
 function findBestMove(board){
-    let bestMove = null;
-    
+    let bestVal = 1000;
+
+    let bestMoveRow = -1;
+    let bestMoveCol = -1;    
+
+    for (let r = 0; r < board.length; r++) {
+        for (let c = 0; c < board[r].length; c++) {
+            if (!board[r][c]) {
+                board[r][c] = ai;
+
+                let moveVal = minimax(board, 0, true);
+
+                board[r][c] = 0;
+            }
+        }
+    }
+
 }
 
-function minimax(board, depth) {
+function minimax(board, depth, isMax) {
     let score = gameState(board);
 
     if (score === 10 || score === -10) return score;
 
-    const best = -1000;
     
-    board.foreach(row => {
-        row.foreach(col => {
-            
-        });
-    });
+    if (isMax) {
+        let best = -1000;
+
+        for (let r = 0; r < board.length; r++) {
+            for (let c = 0; c < board[r].length; c++) {
+                if (!board[r][c]) {
+                    board[r][c] = ai;
+    
+                    best = Math.max(best, minimax(board, depth+1, !isMax));
+
+                    board[r][c] = 0;
+                }
+            }
+        }
+
+        return best;
+    } else {
+        let best = 1000;
+
+        for (let r = 0; r < board.length; r++) {
+            for (let c = 0; c < board[r].length; c++) {
+                if (!board[r][c]) {
+                    board[r][c] = player;
+    
+                    best = Math.min(best, minimax(board, depth+1, !isMax));
+
+                    board[r][c] = 0;
+                }
+            }
+        }
+        return best;
+    }
 }
 
 $('.board-space').on('click', function () {
     const $b = $(this);
-    board[$b.attr('data-row')][$b.attr('data-col')] = -1;
+    board[$b.attr('data-row')][$b.attr('data-col')] = player;
     console.log(board)
     $b.html('<p class="board-content">O');
 });
